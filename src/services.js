918c12ddf89ac91103491a6a06fcbc2c7c73b7db
Peter Savchenko <specc.dev@gmail.com>
2020-04-25 23:24:43 +0300

    add tests, bundle

--- src/services.js
+++ src/services.js
@@ -1,10 +1,11 @@
+/* eslint-disable no-useless-escape */
 export default {
   vimeo: {
     regex: /(?:http[s]?:\/\/)?(?:www.)?vimeo\.co(?:.+\/([^\/]\d+)(?:#t=[\d]+)?s?$)/,
     embedUrl: 'https://player.vimeo.com/video/<%= remote_id %>?title=0&byline=0',
     html: '<iframe style="width:100%;" height="320" frameborder="0"></iframe>',
     height: 320,
-    width: 580
+    width: 580,
   },
   youtube: {
     regex: /(?:https?:\/\/)?(?:www\.)?(?:(?:youtu\.be\/)|(?:youtube\.com)\/(?:v\/|u\/\w\/|embed\/|watch))(?:(?:\?v=)?([^#&?=]*))?((?:[?&]\w*=\w*)*)/,
@@ -21,10 +22,10 @@ export default {
         start: 'start',
         end: 'end',
         t: 'start',
-        'time_continue': 'start',
-        list: 'list'
+        // eslint-disable-next-line camelcase
+        time_continue: 'start',
+        list: 'list',
       };
-
 
       params = params.slice(1)
         .split('&')
@@ -33,66 +34,69 @@ export default {
 
           if (!id && name === 'v') {
             id = value;
+
             return;
           }
 
-          if (!paramsMap[name]) return;
+          if (!paramsMap[name]) {
+            return;
+          }
 
           return `${paramsMap[name]}=${value}`;
         })
         .filter(param => !!param);
 
       return id + '?' + params.join('&');
-    }
+    },
   },
   coub: {
     regex: /https?:\/\/coub\.com\/view\/([^\/\?\&]+)/,
     embedUrl: 'https://coub.com/embed/<%= remote_id %>',
     html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
     height: 320,
-    width: 580
+    width: 580,
   },
   vine: {
     regex: /https?:\/\/vine\.co\/v\/([^\/\?\&]+)/,
     embedUrl: 'https://vine.co/v/<%= remote_id %>/embed/simple/',
     html: '<iframe style="width:100%;" height="320" frameborder="0" allowfullscreen></iframe>',
     height: 320,
-    width: 580
+    width: 580,
   },
   imgur: {
     regex: /https?:\/\/(?:i\.)?imgur\.com.*\/([a-zA-Z0-9]+)(?:\.gifv)?/,
     embedUrl: 'http://imgur.com/<%= remote_id %>/embed',
     html: '<iframe allowfullscreen="true" scrolling="no" id="imgur-embed-iframe-pub-<%= remote_id %>" class="imgur-embed-iframe-pub" style="height: 500px; width: 100%; border: 1px solid #000"></iframe>',
     height: 500,
-    width: 540
+    width: 540,
   },
   gfycat: {
     regex: /https?:\/\/gfycat\.com(?:\/detail)?\/([a-zA-Z]+)/,
     embedUrl: 'https://gfycat.com/ifr/<%= remote_id %>',
     html: "<iframe frameborder='0' scrolling='no' style=\"width:100%;\" height='436' allowfullscreen ></iframe>",
     height: 436,
-    width: 580
+    width: 580,
   },
   'twitch-channel': {
     regex: /https?:\/\/www\.twitch\.tv\/([^\/\?\&]*)\/?$/,
     embedUrl: 'https://player.twitch.tv/?channel=<%= remote_id %>',
     html: '<iframe frameborder="0" allowfullscreen="true" scrolling="no" height="366" style="width:100%;"></iframe>',
     height: 366,
-    width: 600
+    width: 600,
   },
   'twitch-video': {
     regex: /https?:\/\/www\.twitch\.tv\/(?:[^\/\?\&]*\/v|videos)\/([0-9]*)/,
     embedUrl: 'https://player.twitch.tv/?video=v<%= remote_id %>',
     html: '<iframe frameborder="0" allowfullscreen="true" scrolling="no" height="366" style="width:100%;"></iframe>',
     height: 366,
-    width: 600
+    width: 600,
   },
   'yandex-music-album': {
     regex: /https?:\/\/music\.yandex\.ru\/album\/([0-9]*)\/?$/,
     embedUrl: 'https://music\.yandex\.ru/iframe/#album/<%= remote_id %>/',
     html: '<iframe frameborder=\"0\" style=\"border:none;width:540px;height:400px;\" style=\"width:100%;\" height=\"400\"></iframe>',
     height: 400,
-    width: 540
+    width: 540,
   },
   'yandex-music-track': {
     regex: /https?:\/\/music\.yandex\.ru\/album\/([0-9]*)\/track\/([0-9]*)/,
@@ -100,7 +104,7 @@ export default {
     html: '<iframe frameborder="0" style="border:none;width:540px;height:100px;" style="width:100%;" height="100"></iframe>',
     height: 100,
     width: 540,
-    id: (ids) => ids.join('/')
+    id: (ids) => ids.join('/'),
   },
   'yandex-music-playlist': {
     regex: /https?:\/\/music\.yandex\.ru\/users\/([^\/\?\&]*)\/playlists\/([0-9]*)/,
@@ -108,7 +112,7 @@ export default {
     html: '<iframe frameborder="0" style="border:none;width:540px;height:400px;" width="540" height="400"></iframe>',
     height: 400,
     width: 540,
-    id: (ids) => ids.join('/')
+    id: (ids) => ids.join('/'),
   },
   codepen: {
     regex: /https?:\/\/codepen\.io\/([^\/\?\&]*)\/pen\/([^\/\?\&]*)/,
@@ -116,14 +120,14 @@ export default {
     html: "<iframe height='300' scrolling='no' frameborder='no' allowtransparency='true' allowfullscreen='true' style='width: 100%;'></iframe>",
     height: 300,
     width: 600,
-    id: (ids) => ids.join('/embed/')
+    id: (ids) => ids.join('/embed/'),
   },
   instagram: {
     regex: /https?:\/\/www\.instagram\.com\/p\/([^\/\?\&]+)\/?/,
     embedUrl: 'https://www.instagram.com/p/<%= remote_id %>/embed',
     html: '<iframe width="400" height="505" style="margin: 0 auto;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
     height: 505,
-    width: 400
+    width: 400,
   },
   twitter: {
     regex: /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+)(?:\/.*)?$/,
@@ -131,6 +135,6 @@ export default {
     html: '<iframe width="600" height="600" style="margin: 0 auto;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
     height: 300,
     width: 600,
-    id: ids => ids.join('/status/')
- },
+    id: ids => ids.join('/status/'),
+  },
 };
