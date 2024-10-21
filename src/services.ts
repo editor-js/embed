72774147f2f9346aa9a89b789012e6afb33a31ad
Thomas HUMMEL <thomas@hummel.link>
2024-10-21 09:37:41 +0200

    core(services): Keep only expected changes 2

--- src/services.ts
+++ src/services.ts
@@ -131,7 +131,7 @@ const SERVICES: ServicesConfigType = {
     id: (ids) => ids.join('/embed/'),
   },
   instagram: {
-    regex: /https?:\/\/www\.instagram\.com\/p\/([^\/\?\&]+)\/?.*/,
+    regex: /^https:\/\/(?:www\.)?instagram\.com\/(?:reel|p)\/(.*)/,
     embedUrl: 'https://www.instagram.com/p/<%= remote_id %>/embed',
     html: '<iframe width="400" height="505" style="margin: 0 auto;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
     height: 505,
@@ -139,8 +139,8 @@ const SERVICES: ServicesConfigType = {
     id: (groups: string[]) => groups?.[0]?.split("/")[0],
   },
   twitter: {
-    regex: /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+?.*)?$/,
-    embedUrl: 'https://twitframe.com/show?url=https://twitter.com/<%= remote_id %>',
+    regex: /^https?:\/\/(www\.)?(?:twitter\.com|x\.com)\/.+\/status\/(\d+)/,
+    embedUrl: 'https://platform.twitter.com/embed/Tweet.html?id=<%= remote_id %>',
     html: '<iframe width="600" height="600" style="margin: 0 auto;" frameborder="0" scrolling="no" allowtransparency="true"></iframe>',
     height: 300,
     width: 600,
